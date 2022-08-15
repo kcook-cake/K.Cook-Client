09676a1c95df517d3de12d01e9500b4b1c98dda2
choiyeju <yeju1019@gmail.com>
2022-08-16 01:33:42 +0900

    Modify: 케이크(Cake), 스토어(Store) 완료

--- src/utils/getAxios.js
+++ src/utils/getAxios.js
@@ -46,7 +46,37 @@ const getAxios = (fn, fnPage, link, list, num, page, pageAdd) => {
             fn(changeData);
         })
         .catch((error) => {
-            
+            // const data = res.data.result.content;
+            // fnPage(data.length);
+
+            var num1 = page*num+pageAdd;
+            // var num2 = data.length;
+            // var num3 = (page+1)*num+pageAdd;
+            // if (num2 > num3) num2 = num3;
+
+            var changeData = [];
+            // for (var i = num1; i < num2; i++) {
+            //     changeData[i] = res.data.result.content[i];
+            //     changeData[i] += {
+            //         num: i,
+            //     };
+            // }
+            for (var i = 0; i < (page+1)*num+pageAdd; i++) {
+                changeData[i] = {
+                    isCake: true,
+                    name: "~준비중 입니다~",
+                    price: 0,
+                    productId: 0,
+                    raiting: ".00",
+                    resultPrice: 0,
+                    reviewCount: 0,
+                    salePrice: 0,
+                    status: "VALID",
+                    storeName: "~준비중 입니다~",
+                    thumbnail: "",
+                }
+            }
+            fn(changeData);
         });
 }
 
