2ab09a138d783b12563439e0f40b7a7f4895f2f9
Jho2e <ohjaehokor@naver.com>
2022-08-26 10:53:48 +0900

    Modify: 스토어(/Store)
    Revert To Subway ,City
    
    매끄럽게 변경

--- src/containers/main/store/Store.tsx
+++ src/containers/main/store/Store.tsx
@@ -25,26 +25,24 @@ function Store() {
   //선택지 가로 위치 계산
   const [width, setWidth] = useState(0);
   const SelectCloseF = () => {
-      var n1: any = $(".cake-flex").width();
-      var n2: any = $(".cake").width();
-      if ((n1-n2)/2 < 0) setWidth(0);
-      else setWidth((n1-n2)/2);
+    var n1: any = $('.cake-flex').width();
+    var n2: any = $('.cake').width();
+    if ((n1 - n2) / 2 < 0) setWidth(0);
+    else setWidth((n1 - n2) / 2);
 
-      for (var i=1; i<5; i++) {
-          selectWindow[i][0] = false;
-        }
-        setNum(num+1);
+    for (var i = 1; i < 5; i++) {
+      selectWindow[i][0] = false;
+    }
+    setNum(num + 1);
   };
-
-
 
   //선택지창
   const [selectWindow, setSelectWindow] = useState([
-    [false, "정렬", 0],
-    [false, "지역", 0],
-    [false, "시/군", 0],
-    [false, "지하철", 0],
-    [false, "", 0],
+    [false, '정렬', 0],
+    [false, '지역', 0],
+    [false, '시/군', 0],
+    [false, '지하철', 0],
+    [false, '', 0],
   ]);
   //선택지바
   const [selectAll, setSelectAll] = useState([]);
@@ -54,9 +52,6 @@ function Store() {
 
   //더보기
   const [cakeDetail, setCakeDetail] = useState(true);
-
-
-
 
   /* 지역클릭->시/군 보이게 & 지하철흐리게 */
   const [cityBtnOn, setCityBtnOn] = useState(false);
@@ -69,37 +64,58 @@ function Store() {
   // 초기화 버튼
   // 배열초기화는 주석처리해둠 <-- 넣어두면 자동초기화할때 무한루프
   const onResetCakeSelect = () => {
-    selectWindow[1][1] = "지역";
-    selectWindow[2][1] = "시/군";
-    selectWindow[3][1] = "지하철";
-    for (var i=1; i<4;i++)
-      selectWindow[i][3] = 0;
+    selectWindow[1][1] = '지역';
+    selectWindow[2][1] = '시/군';
+    selectWindow[3][1] = '지하철';
+    for (var i = 1; i < 4; i++) selectWindow[i][3] = 0;
     SelectCloseF();
   };
 
-  // 아래 두개 리버트 기능
-  // 동작할때 지직거리는것 때문에
-  // 비동기로 바꿀 예정
+  // 지역 <ㅡ> 지하철 리버트 기능
 
+  // 리버트 기능 생성
+  async function revertToSubway() {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(true), 500);
+      setCityBtnOn(false);
+      selectWindow[1][1] = '지역';
+    });
+
+    let result = await promise; // 프라미스가 이행될 때까지 기다림 (*)
+    selectWindow[3][0] = Boolean(result);
+  }
+  // setSelectAll([]);
+  async function revertToCity() {
+    let promise = new Promise((resolve, reject) => {
+      //      setSelectAll([]);
+      setTimeout(() => resolve(true), 1000);
+      setSubwayBtnOn(false);
+      selectWindow[3][1] = '지하철';
+    });
+
+    let result = await promise; // 프라미스가 이행될 때까지 기다림 (*) result는 0.5초후에 "true"가 할당됨.
+    selectWindow[2][0] = Boolean(result);
+  }
+
+  // onClick에 넣을 리버트 조건문 생성
   const onRevertToSubway = () => {
     if (
       cityBtnOn === true ||
-      (Array.isArray(selectAll) && selectAll.length !== 0 && cityBtnOn) ||
-      selectWindow[1][1] !== "지역"
+      (Array.isArray(selectAll) && selectAll.length !== 0) ||
+      selectWindow[1][1] !== '지역'
     ) {
-      setSelectAll([]);
+      revertToSubway();
       //   setSelectFourShow(true);
       //      setCityBtnOn(false);
     }
   };
-
   const onRevertToCity = () => {
     if (
       subwayBtnOn === true ||
       (Array.isArray(selectAll) && selectAll.length !== 0 && subwayBtnOn) ||
-      selectWindow[3][1] !== "지하철"
+      selectWindow[3][1] !== '지하철'
     ) {
-      setSelectAll([]);
+      revertToCity();
       //      setSelectTwoShow(true);
     }
   };
@@ -113,9 +129,6 @@ function Store() {
       setSubwayBtnOn(false);
     }
   }, [selectAll]);
-
-  
-
 
   const [resize, setResize] = useState(0);
   const handleResize = () => {
@@ -139,23 +152,28 @@ function Store() {
     <>
       <div className="cake-flex store-flex">
         {/* 선택지창 */}
-        {resize>767 || selectMobileTF?
-            <SelectWindow
-                cakestoreTF={false} width={width} height={250}
-                num={num} setNumF={setNum}
-                setSelectMobileTF={setSelectMobileTF} SelectCloseF={SelectCloseF}
-                selectAll={selectAll} selectWindow={selectWindow} />
-            :null
-        }
+        {resize > 767 || selectMobileTF ? (
+          <SelectWindow
+            cakestoreTF={false}
+            width={width}
+            height={250}
+            num={num}
+            setNumF={setNum}
+            setSelectMobileTF={setSelectMobileTF}
+            SelectCloseF={SelectCloseF}
+            selectAll={selectAll}
+            selectWindow={selectWindow}
+          />
+        ) : null}
 
         <div className="cake store">
           <div className="cake-select-flex">
-            
             <div
               className="mobile cake-search"
               onClick={() => {
                 setSelectMobileTF(true);
-              }}>
+              }}
+            >
               <img src={search} />
               상세검색
             </div>
@@ -172,19 +190,20 @@ function Store() {
                       SelectCloseF();
                       if (selectWindow[0][0]) selectWindow[0][0] = false;
                       else selectWindow[0][0] = true;
-                      setNum(num+1);
+                      setNum(num + 1);
                     }}
                   />
                 </div>
               </div>
             </div>
-            
+
             {/* PC-지역 버튼 */}
             <div
               className={classNames('pc', 'cake-select', {
                 use: cityBtnOn || selectWindow[1][1] !== '지역',
                 nouse: subwayBtnOn || selectWindow[3][1] !== '지하철',
-              })}>
+              })}
+            >
               <div style={{ display: 'flex' }}>
                 <button id="cake-select-three" className="cake-select-button">
                   {selectWindow[1][1]}
@@ -198,10 +217,11 @@ function Store() {
                       SelectCloseF();
                       if (selectWindow[1][0]) selectWindow[1][0] = false;
                       else selectWindow[1][0] = true;
-                      setNum(num+1);
+                      setNum(num + 1);
 
-                      setCityBtnOn((prev) => !prev);
-                      setSubwayBtnOn(false);
+                      //                      setCityBtnOn((prev) => !prev);
+                      // setSubwayBtnOn(false);
+                      console.log('subwayBtnOn', subwayBtnOn);
                     }}
                   />
                 </div>
@@ -215,14 +235,15 @@ function Store() {
                   SelectCloseF();
                   if (selectWindow[2][0]) selectWindow[2][0] = false;
                   else selectWindow[2][0] = true;
-                  setNum(num+1);
-                }}>
+                  setNum(num + 1);
+                }}
+              >
                 <div style={{ display: 'flex' }}>
                   <button id="cake-select-two" className="cake-select-button">
                     {selectWindow[2][1]}
                   </button>
                   <div className="cake-select-img">
-                    <img src={selectAllow}/>
+                    <img src={selectAllow} />
                   </div>
                 </div>
               </div>
@@ -235,51 +256,55 @@ function Store() {
               })}
               onClick={() => {
                 onRevertToSubway();
-                //
+
                 setSubwayBtnOn((prev) => !prev);
                 setCityBtnOn(false);
 
                 SelectCloseF();
                 if (selectWindow[3][0]) selectWindow[3][0] = false;
                 else selectWindow[3][0] = true;
-                setNum(num+1);
-              }}>
+                setNum(num + 1);
+              }}
+            >
               <div style={{ display: 'flex' }}>
                 <button id="cake-select-four" className="cake-select-button">
                   {selectWindow[3][1]}
                 </button>
                 <div className="cake-select-img">
-                  <img src={selectAllow} alt="cake-select-img"/>
+                  <img src={selectAllow} alt="cake-select-img" />
                 </div>
               </div>
             </div>
 
             {/* 선택지 바 */}
-            {(resize>767 || selectMobileTF) && selectAll.length != 0?
+            {(resize > 767 || selectMobileTF) && selectAll.length != 0 ? (
+              <div
+                className="cake-select-bar"
+                style={{
+                  marginTop: resize > 767 ? 10 : 50 * 2,
+                }}
+              >
+                <SelectBar setSelectAllF={setSelectAll} getData={selectAll} />
                 <div
-                    className="cake-select-bar"
-                    style={{
-                        marginTop: resize>767? 10 : 50*2,
-                    }}>
-                    <SelectBar setSelectAllF={setSelectAll} getData={selectAll} />
-                    <div
-                        className="cake-bar-card-all-delete"
-                        onClick={()=>{
-                            SelectCloseF();
+                  className="cake-bar-card-all-delete"
+                  onClick={() => {
+                    SelectCloseF();
 
-                            selectWindow[1][1] = "지역";
-                            selectWindow[2][1] = "시/군";
-                            selectWindow[3][1] = "지하철";
-                            
-                            for (var i=1; i<4; i++) {
-                              selectWindow[i][2] = 0;
-                            }
-        
-                            setSelectAll([]);
-                        }}
-                    >초기화</div>
+                    selectWindow[1][1] = '지역';
+                    selectWindow[2][1] = '시/군';
+                    selectWindow[3][1] = '지하철';
+
+                    for (var i = 1; i < 4; i++) {
+                      selectWindow[i][2] = 0;
+                    }
+
+                    setSelectAll([]);
+                  }}
+                >
+                  초기화
                 </div>
-            :null}
+              </div>
+            ) : null}
           </div>
 
           <div className="cake-store-contents store-contents-flex">
