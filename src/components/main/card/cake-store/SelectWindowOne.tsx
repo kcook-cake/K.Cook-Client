0ee061e2a9815472a3d4a998d8b458168295b338
Jho2e <ohjaehokor@naver.com>
2022-08-31 14:52:17 +0900

    Modify: 케이크(/Cake) 인기순
    axios get 변경필요,
    
    get 실행함수
    "cake-select-ul" 인기순 li에 onclick으로 넣을 예정

--- src/components/main/card/cake-store/SelectWindowOne.tsx
+++ src/components/main/card/cake-store/SelectWindowOne.tsx
@@ -1,48 +1,55 @@
-import { useState } from "react";
+import { useState } from 'react';
 import classNames from 'classnames';
-import "src/styles/main/card/cake-store/SelectWindow.scss";
+import 'src/styles/main/card/cake-store/SelectWindow.scss';
 
-import X from "src/assets/x.svg";
+import X from 'src/assets/x.svg';
 
 interface Props {
-    width: any,
-    NumF: any,
-    selectWindow: any,
-    selectDataOne: any,
+  width: any;
+  NumF: any;
+  selectWindow: any;
+  selectDataOne: any;
 }
 
-export default function SelectWindowOne({ width, NumF, selectWindow, selectDataOne, }: Props) {
+export default function SelectWindowOne({
+  width,
+  NumF,
+  selectWindow,
+  selectDataOne,
+}: Props) {
+  const SelectOneF = (str: string, num: number, length: number) => {
+    $('#cake-select-li-' + num).css('color', '#ea5450');
+    selectWindow[0] = [false, str, length];
+    NumF();
+  };
 
-    const SelectOneF = (str: string, num: number, length: number) => {
-        $("#cake-select-li-"+num).css("color", "#ea5450");
-        selectWindow[0] = [false, str, length];
-        NumF();
-    };
-
-    return (
-        <>
-            {selectWindow[0][0]?
-                <div
-                    className="cake-select-absolute-one"
-                    style={{ left: (width+1060)+"px", }}>
-                    <div className="cake-select-top"></div>
-                    <ul className="cake-select-ul">
-                        {selectDataOne[0].map((data: any, idx: any, )=>{
-                            return (
-                                <li
-                                    className={classNames("cake-select-li", {
-                                        "cake-select-li-top": idx == 0,
-                                        "cake-select-li-bottom": idx == selectDataOne[0].length-1,
-                                    })}
-                                    onClick={()=>{SelectOneF(data, idx+1, 8*data.length);}}>
-                                    {data}
-                                </li>
-                            )
-                            })
-                        }
-                    </ul>
-                </div>
-            :null}
-        </>
-    );
+  return (
+    <>
+      {selectWindow[0][0] ? (
+        <div
+          className="cake-select-absolute-one"
+          style={{ left: width + 1060 + 'px' }}
+        >
+          <div className="cake-select-top"></div>
+          <ul className="cake-select-ul">
+            {selectDataOne[0].map((data: any, idx: any) => {
+              return (
+                <li
+                  className={classNames('cake-select-li', {
+                    'cake-select-li-top': idx == 0,
+                    'cake-select-li-bottom': idx == selectDataOne[0].length - 1,
+                  })}
+                  onClick={() => {
+                    SelectOneF(data, idx + 1, 8 * data.length);
+                  }}
+                >
+                  {data}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : null}
+    </>
+  );
 }
--- src/containers/main/cake/Cake.tsx
+++ src/containers/main/cake/Cake.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import axios from 'axios';
 import $ from 'jquery';
 import 'src/styles/main/CakeStore.scss';
 
-import search from "../../../assets/search.svg";
-import selectAllow from "../../../assets/selectArrow.png";
-import X from "../../../assets/x.svg";
+import search from '../../../assets/search.svg';
+import selectAllow from '../../../assets/selectArrow.png';
+import X from '../../../assets/x.svg';
 
 import getAxios from 'src/utils/getAxios';
 import PickCard from 'src/components/main/card/PickCard';
@@ -19,176 +19,200 @@ import SelectBox from 'src/components/main/card/cake-store/SelectBox';
 import SelectBoxOne from 'src/components/main/card/cake-store/SelectBoxOne';
 import SelectWindowOne from 'src/components/main/card/cake-store/SelectWindowOne';
 
-function Cake (){
-    const [num, setNum] = useState(0);
-    const NumF = () => {
-        setNum(num + 1);
-    };
+function Cake() {
+  const [num, setNum] = useState(0);
+  const NumF = () => {
+    setNum(num + 1);
+  };
 
-    //선택지 가로 위치 계산
-    const [width, setWidth] = useState(0);
-    const SelectCloseF = () => {
-        var n1: any = $(".cake-flex").width();
-        var n2: any = $(".cake").width();
-        if ((n1-n2)/2 < 0) setWidth(0);
-        else setWidth((n1-n2)/2);
+  //선택지 가로 위치 계산
+  const [width, setWidth] = useState(0);
+  const SelectCloseF = () => {
+    var n1: any = $('.cake-flex').width();
+    var n2: any = $('.cake').width();
+    if ((n1 - n2) / 2 < 0) setWidth(0);
+    else setWidth((n1 - n2) / 2);
 
-        for (var i=1; i<5; i++) {
-            selectWindow[i][0] = false;
-          }
-          setNum(num+1);
-    };
+    for (var i = 1; i < 5; i++) {
+      selectWindow[i][0] = false;
+    }
+    setNum(num + 1);
+  };
 
+  const [selectDataOne, setSelectDataOne] = useState([
+    ['인기순', '최신순', '판매량순', '낮은 가격순', '높은 가격순'],
+  ]);
+  const [selectData, setSelectData] = useState([[], [], [], []]);
 
+  //선택지창
+  const [selectWindow, setSelectWindow] = useState([
+    [false, '정렬', 0],
+    [false, '지역', 28],
+    [false, '맛', 14],
+    [false, '이벤트', 42],
+    [false, '가격대', 42],
+  ]);
+  //선택지바
+  const [selectAll, setSelectAll] = useState([]);
 
-    const [selectDataOne, setSelectDataOne] = useState([["인기순", "최신순", "판매량순", "낮은 가격순", "높은 가격순"]]);
-    const [selectData, setSelectData] = useState([[], [], [], []]);
+  //선택지 모바일
+  const [selectMobileTF, setSelectMobileTF] = useState(false);
+  //더보기
+  const [cakeDetail, setCakeDetail] = useState(true);
 
-    //선택지창
-    const [selectWindow, setSelectWindow] = useState([
-        [false, "정렬", 0],
-        [false, "지역", 28],
-        [false, "맛", 14],
-        [false, "이벤트", 42],
-        [false, "가격대", 42],
-    ]);
-    //선택지바
-    const [selectAll, setSelectAll] = useState([]);
+  const [resize, setResize] = useState(0);
+  const handleResize = () => {
+    setResize(window.innerWidth);
+  };
 
-    //선택지 모바일
-    const [selectMobileTF, setSelectMobileTF] = useState(false);
-    //더보기
-    const [cakeDetail, setCakeDetail] = useState(true);
-    
-    
+  const [data, setData] = useState([]);
+  //0페이지부터 시작한다
+  const [pageTodays, setPageTodays] = useState(0);
+  const [lengthTodays, setLengthTodays] = useState(0);
+  useEffect(() => {
+    LinkClick('Cake');
 
-    const [resize, setResize] = useState(0);
-    const handleResize = () => {
-      setResize(window.innerWidth);
-    };
+    setResize(window.innerWidth);
+    window.addEventListener('resize', handleResize);
 
-    const [data, setData] = useState([]);
-    //0페이지부터 시작한다
-    const [pageTodays, setPageTodays] = useState(0);
-    const [lengthTodays, setLengthTodays] = useState(0);
-    useEffect(() => {
-        LinkClick("Cake");
+    getAxios(setData, setLengthTodays, 'cakes', [], 24, pageTodays, 0);
+    axios.get(`https://prod.kcook-cake.com/app/cities`).then((res) => {
+      setSelectData([
+        res.data.result,
+        ['생크림', '크림치즈', '버터크림', '앙금'],
+        [
+          '친구/애인',
+          '부모님',
+          '어린이',
+          '할로윈데이',
+          '빼빼로데이',
+          '크리스마스',
+          '입학/졸업/취업',
+          '명절',
+          '발렌타인/화이트데이',
+          '어버이날/스승의날',
+          '돌/환갑/칠순잔치',
+          '프로포즈/브라이덜샤워',
+          '연애인',
+          '기업',
+        ],
+        ['~3만 원', '3~5만 원', '5~7만 원', '7~10만 원', '10만 원~'],
+      ]);
+    });
+  }, []);
 
-        setResize(window.innerWidth);
-        window.addEventListener('resize', handleResize);
+  return (
+    <>
+      <div className="cake-flex">
+        {/* 선택지창 */}
+        <SelectWindowOne
+          width={width}
+          NumF={NumF}
+          selectWindow={selectWindow}
+          selectDataOne={selectDataOne}
+        />
+        {resize > 767 || selectMobileTF ? (
+          <SelectWindow
+            cakestoreTF={true}
+            width={width}
+            height={250}
+            NumF={NumF}
+            selectAll={selectAll}
+            selectBox={[false, false, false]}
+            selectWindow={selectWindow}
+            selectDataOne={selectDataOne}
+            selectData={selectData}
+            setSelectMobileTF={setSelectMobileTF}
+            SelectCloseF={SelectCloseF}
+            setSelectAllF={setSelectAll}
+          />
+        ) : null}
 
-        getAxios(setData, setLengthTodays, "cakes", [], 24, pageTodays, 0);
-        axios
-            .get(`https://prod.kcook-cake.com/app/cities`)
-            .then(res =>{
-                setSelectData([res.data.result, [
-                    "생크림",
-                    "크림치즈",
-                    "버터크림",
-                    "앙금",
-                ], [
-                    "친구/애인",
-                    "부모님",
-                    "어린이",
-                    "할로윈데이",
-                    "빼빼로데이",
-                    "크리스마스",
-                    "입학/졸업/취업",
-                    "명절",
-                    "발렌타인/화이트데이",
-                    "어버이날/스승의날",
-                    "돌/환갑/칠순잔치",
-                    "프로포즈/브라이덜샤워",
-                    "연애인",
-                    "기업"
-                ], [
-                    "~3만 원",
-                    "3~5만 원",
-                    "5~7만 원",
-                    "7~10만 원",
-                    "10만 원~",
-                ]]);
-            });
-    },[]);
-
-    return(
-        <>
-        <div className="cake-flex">
-            {/* 선택지창 */}
-            <SelectWindowOne width={width} NumF={NumF} selectWindow={selectWindow} selectDataOne={selectDataOne} />
-            {resize>767 || selectMobileTF?
-                <SelectWindow
-                    cakestoreTF={true} width={width} height={250} NumF={NumF}
-                    selectAll={selectAll} selectBox={[false, false, false]} selectWindow={selectWindow}
-                    selectDataOne={selectDataOne} selectData={selectData}
-                    setSelectMobileTF={setSelectMobileTF} SelectCloseF={SelectCloseF} setSelectAllF={setSelectAll}/>
-            :null}
-
-            <div className="cake">
-                <div className="cake-select-flex">
-                    <div
-                        className="mobile cake-search"
-                        onClick={()=>{
-                            setSelectMobileTF(true);
-                        }}>
-                        <img src={search} />
-                        상세검색
-                    </div>
-
-                    {/* 선택지 박스 */}
-                    <SelectBoxOne selectWindow={selectWindow} SelectCloseF={SelectCloseF} />
-                    <SelectBox 
-                        cakestoreTF={true} NumF={NumF}
-                        selectBox={[false, false, false, false]} selectWindow={selectWindow} 
-                        SelectCloseF={SelectCloseF} setSelectAllF={setSelectAll}/>
-
-                    {/* 선택지 바 */}
-                    {selectAll.length != 0?
-                        <div
-                            className="cake-select-bar">
-                            <SelectBar setSelectAllF={setSelectAll} getData={selectAll} />
-                            <div
-                                className="cake-bar-card-all-delete"
-                                onClick={()=>{
-                                    SelectCloseF();
-
-                                    selectWindow[1][1] = "지역";
-                                    selectWindow[2][1] = "맛";
-                                    selectWindow[3][1] = "이벤트";
-                                    selectWindow[4][1] = "가격대";
-                                    
-                                    selectWindow[1][2] = 28;
-                                    selectWindow[2][2] = 14;
-                                    selectWindow[3][2] = 42;
-                                    selectWindow[4][2] = 42;
-                
-                                    setSelectAll([]);
-                                }}
-                            >초기화</div>
-                        </div>
-                    :null}
-                </div>
-
-                <div className="cake-store-contents cake-contents-flex">
-                    <CakeStoreTitle detail={cakeDetail} setDetailF={setCakeDetail}/>
-                    <div className="contents">
-                        <CakeCard getData={data} cakeDetail={cakeDetail} />
-                    </div>
-                    <div className={"pagination cake-store-"+(cakeDetail? "dummy": "")}>
-                        <Link to="/" className="arrow prev" href="#"> &lt;Prev</Link>
-                        <Link to="/" href="#" className="pagi active">1</Link>
-                        <Link to="/" href="#" className="pagi" >2</Link>
-                        <Link to="/" href="#" className="pagi" >3</Link>
-                        <Link to="/" className="arrow next" href="#">Next &gt;</Link>
-                    </div>
-                </div>
-                <PickCard/>
+        <div className="cake">
+          <div className="cake-select-flex">
+            <div
+              className="mobile cake-search"
+              onClick={() => {
+                setSelectMobileTF(true);
+              }}
+            >
+              <img src={search} />
+              상세검색
             </div>
+
+            {/* 선택지 박스 */}
+            <SelectBoxOne
+              selectWindow={selectWindow}
+              SelectCloseF={SelectCloseF}
+            />
+            <SelectBox
+              cakestoreTF={true}
+              NumF={NumF}
+              selectBox={[false, false, false, false]}
+              selectWindow={selectWindow}
+              SelectCloseF={SelectCloseF}
+              setSelectAllF={setSelectAll}
+            />
+
+            {/* 선택지 바 */}
+            {selectAll.length != 0 ? (
+              <div className="cake-select-bar">
+                <SelectBar setSelectAllF={setSelectAll} getData={selectAll} />
+                <div
+                  className="cake-bar-card-all-delete"
+                  onClick={() => {
+                    SelectCloseF();
+
+                    selectWindow[1][1] = '지역';
+                    selectWindow[2][1] = '맛';
+                    selectWindow[3][1] = '이벤트';
+                    selectWindow[4][1] = '가격대';
+
+                    selectWindow[1][2] = 28;
+                    selectWindow[2][2] = 14;
+                    selectWindow[3][2] = 42;
+                    selectWindow[4][2] = 42;
+
+                    setSelectAll([]);
+                  }}
+                >
+                  초기화
+                </div>
+              </div>
+            ) : null}
+          </div>
+
+          <div className="cake-store-contents cake-contents-flex">
+            <CakeStoreTitle detail={cakeDetail} setDetailF={setCakeDetail} />
+            <div className="contents">
+              <CakeCard getData={data} cakeDetail={cakeDetail} />
+            </div>
+            <div
+              className={'pagination cake-store-' + (cakeDetail ? 'dummy' : '')}
+            >
+              <Link to="/" className="arrow prev" href="#">
+                {' '}
+                &lt;Prev
+              </Link>
+              <Link to="/" href="#" className="pagi active">
+                1
+              </Link>
+              <Link to="/" href="#" className="pagi">
+                2
+              </Link>
+              <Link to="/" href="#" className="pagi">
+                3
+              </Link>
+              <Link to="/" className="arrow next" href="#">
+                Next &gt;
+              </Link>
+            </div>
+          </div>
+          <PickCard />
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
-
-
 
 export default Cake;
