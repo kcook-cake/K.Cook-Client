58e3a862dfa2ff0570c4a8e65df7a139e4beb431
choiyeju <yeju1019@gmail.com>
2022-09-25 21:57:50 +0900

    Cake: Modify: 케이크 디테일 페이지 수정

--- src/components/detail/cake/OptionList.tsx
+++ src/components/detail/cake/OptionList.tsx
@@ -14,27 +14,46 @@ interface Props {
 }
 
 function OptionList({getData}: Props) {
+    const [select, setSelect] = useState(0);
+    let [output, setOutput] = useState([]);
+    let [child, setChild] = useState([]);
+
+
+
     useEffect(()=>{
     },[]);
 
     return (
         <>
-            {getData.map((data:{ optionId: any, optionName: any, optionList: any, }, idx: any, )=>{
+            {getData.map((option:{ optionId: any, optionNumber: any, optionName: any, itemList: any, }, idx: any, )=>{
                 return (
                     <>
-                        <div className='cake-detail-optionlist-flex'>
-                            <div style={{ marginBottom: "6px", }}>{(idx+1)+". "+data.optionName}</div>
+                        <div
+                            className='cake-detail-optionlist-flex'
+                            style={{ opacity: (select<idx? "0.4": "1"), }}>
+                            <div style={{ marginBottom: "6px", }}>{(idx+1)+". "+option.optionName}</div>
                             <div className='cake-detail-optionlist-select'>
-                                <select>
-                                    <option disabled>옵션 선택</option>
-                                    {data.optionList.map(( option:{optionListId: any, optionListName: any, optionListPrice: any,} )=>{
-                                        return (
-                                            <option>
-                                                {option.optionListName}
-                                            </option>
-                                        );
-                                    })}
-                                </select>
+                                {/* <select
+                                    onChange={(e)=>{
+                                        output[idx] = e.target.value; setOutput(output);
+                                        setSelect(idx+1);
+                                    }}>
+                                </select> */}
+                                {/* <div>옵션 선택</div> */}
+                                {option.itemList.map(( item:{itemId: any, itemNumber: any, itemType: any, itemName: any, itemPrice: any, itemChild: any, } )=>{
+                                    return (
+                                        <div
+                                            style={{ width: "100%", height: "10px", background: "green" }}
+                                            onClick={()=>{
+                                                if (item.itemChild === undefined) child = [];
+                                                else child = item.itemChild;
+                                                setChild(child);
+                                                console.log(child);
+                                            }}>
+                                            {item.itemName}
+                                        </div>
+                                    );
+                                })}
                             </div>
                             <div className='cake-detail-optionlist-btn'>
                                 <div></div>
--- src/utils/List2Option.js
+++ src/utils/List2Option.js
@@ -3,29 +3,36 @@ import axios from "axios";
 import $, { get } from "jquery";
 
 const List2Option = (getData) => {
+    console.log(getData);
     const category = {
-        "SIZE": "크기",
-        "TASTE": "맛",
-        "COLOR": "색상",
-        "DESIGN": "디자인",
-        "SIDE_DECO": "사이드데코",
-        "DECO": "데코",
-        "LOWER_LETTERING": "레터링",
-        "FONT": "글씨체",
-        "WRITE_SIZE": "글씨크기",
-        "PHOTO": "사진",
-        "PACKAGE": "포장",
-        "CANDLE": "초",
-        "ETC": "기타",
+        "SIZE": "크기&0",
+        "TASTE": "맛&1",
+        "COLOR": "색상&2",
+        "DESIGN": "디자인&3",
+        "SIDE_DECO": "사이드데코&4",
+        "DECO": "데코&5",
+        "LOWER_LETTERING": "레터링&6",
+        "FONT": "글씨체&7",
+        "WRITE_SIZE": "글씨크기&8",
+        "PHOTO": "사진&9",
+        "PACKAGE": "포장&10",
+        "CANDLE": "초&11",
+        "ETC": "기타&12",
     };
 
     var chOptionList = [];
+    var chOptionListTF = [];
+    for (var i=0; i<13; i++) chOptionListTF[i] = true;
     
     var j = -1;
     for (var i=0; i<getData.length; i++) {
         var c = category[getData[i].category];
+        // var c = category[getData[i].category].split("&")[0];
+        var oid = category[getData[i].category].split("&")[1];
+        console.log(c);
 
-        if (getData[i].itemNumber === 0) {
+        // if (getData[i].itemNumber === 0) {
+        if (chOptionListTF[oid]) {
             chOptionList.push({
                 optionId: true,
                 optionNumber: chOptionList.length,
@@ -33,25 +40,31 @@ const List2Option = (getData) => {
                 itemList: [{
                     itemId: getData[i].optionsId,
                     itemNumber: 0,
-                    itemType: getData[i].itemType,
+                    itemType: "normal",
+                    // itemType: getData[i].itemType,
                     itemName: getData[i].contents,
                     itemPrice: getData[i].additionalCost,
                     itemChild: getData[i].childOptionsList,
                 }],
             });
+            chOptionListTF[oid] = false;
             j++;
         }
         else {
             chOptionList[j].itemList.push({
                 itemId: getData[i].optionsId,
-                itemNumber: chOptionList[j].itemList.length,
-                itemType: getData[i].itemType,
+                itemNumber: 0,
+                itemNumber: 1,
+                // itemNumber: chOptionList[j].itemList.length,
+                // itemType: getData[i].itemType,
+                itemType: "normal",
                 itemName: getData[i].contents,
                 itemPrice: getData[i].additionalCost,
                 itemChild: getData[i].child,
             })
         }
     }
+    console.log(chOptionList);
     return chOptionList;
 };
 
